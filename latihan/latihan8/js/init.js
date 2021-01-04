06c6e1f641fc2746c1c003cfdc12da4f6cb0a386
calvinawuysan <vinac2625@gmail.com>
2021-01-04 13:11:02 +0800

    latihan 8 matakuliah DEWEB201

---
+++ latihan/latihan8/js/init.js
@@ -0,0 +1,15 @@
+
+(function($) {
+  $(document).ready(function() {
+    $('input#input_text, textarea#deskripsi').characterCounter();
+  });
+})(jQuery);
+
+
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
